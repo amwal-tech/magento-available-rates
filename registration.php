d4e5099c6fad3a672a352c264516e20bc37bb175
Ahmed Mardi <Mardi@amwal.tech>
2024-04-17 12:29:09 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Amwal_AvailableRates'
+    'Amwal_AvailableRates',
     __DIR__
 );
