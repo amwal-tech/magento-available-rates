f7eb9231664ac14918ae73e3d77bb3281f7aabd7
Ahmed Mardi <Mardi@amwal.tech>
2024-05-07 12:57:42 +0300

    WIP

--- Plugin/Button/SetAvailableRates.php
+++ Plugin/Button/SetAvailableRates.php
@@ -21,6 +21,21 @@ class SetAvailableRates
      */
     public function afterGetAvailableRates(GetQuote $subject, array $result, Quote $quote): array
     {
+        /**
+         * Adjust $result to provide the shipping methods as you wish to show them in the Amwal modal.
+         * The returned array must have the following format:
+         * [
+         *      [ID OF SHIPPING METHOD] => [
+         *          'carrier_title' => [TITLE OF SHIPPING METHOD],
+         *          'price' => [PRICE OF SHIPPING]
+         *      ]
+         * ];
+         *
+         * Where:
+         * [ID OF SHIPPING METHOD] = The ID used for the shipping method in magento. Usually a combination of: carrier code and method name.
+         * [TITLE OF SHIPPING METHOD] = The title that should be shown in the Amwal modal
+         * [PRICE OF SHIPPING] = The amount that should be paid for the shipping method
+         */
         $filteredRates = [];
         foreach ($result as $key => $rate) {
             if (!$this->containsCarrierCode($key)) {
